32b8955fac059aa9922ef3141bc9814404f708e2
BlackFox <fiona.speranskih@yandex.ru>
2022-03-11 17:46:55 +0500

    Добавьте файлы проекта.

---
+++ Client/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new byte[1024];
+            const int serverPort = 52000;
+            const int clientPort = 54000;
+            const string ip = "127.0.0.1";
+
+            Console.Write("Введите имя файла, который хотите сохранить на сервере: ");
+            string name = Console.ReadLine();
+
+            var clientEndPoint = new IPEndPoint(IPAddress.Parse(ip), clientPort);
+            EndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse(ip), serverPort);
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            socket.Bind(clientEndPoint);
+
+            using (var fstream = new FileStream(name, FileMode.Open))
+            {
+                while (fstream.Read(data, 0, data.Length) > 0)
+                {
+                    socket.SendTo(data, serverEndPoint);
+                }
+            }
+
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Program
+    {
+        static int size;
+        static Socket socket;
+        static EndPoint clientEndPoint;
+        static IPEndPoint serverEndPoint;
+
+        const int serverPort = 52000;
+        const int clientPort = 54000;
+        const string ip = "127.0.0.1";
+        static byte[] data = new byte[1024];
+
+        static void Main(string[] args)
+        {
+            Initialise();
+            Listening();
+        }
+
+        static void Initialise()
+        {
+            serverEndPoint = new IPEndPoint(IPAddress.Parse(ip), serverPort);
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            socket.Bind(serverEndPoint);
+        }
+
+        static void Listening()
+        {
+            clientEndPoint = new IPEndPoint(IPAddress.Parse(ip), clientPort);
+
+            using (FileStream fstream = new FileStream("Image.png", FileMode.OpenOrCreate))
+            {
+                do
+                {
+                    size = socket.ReceiveFrom(data, ref clientEndPoint);
+                    fstream.Write(data, 0, size);
+
+                } while (socket.Available > 0);
+            }
+
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+        }
+    }
+}
